af769c4188e7e9fe4336c766fd353052c06c05ab
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-05 00:06:22 +0500

    Modify as per linters

--- Script/script.js
+++ Script/script.js
@@ -1,18 +1,18 @@
-const openBtn = document.querySelector("#menuBtn");
-const closeBtn = document.querySelector("#closeBtn");
-const mobMenu = document.querySelector("#mob-navigation");
+const openBtn = document.querySelector('#menuBtn');
+const closeBtn = document.querySelector('#closeBtn');
+const mobMenu = document.querySelector('#mob-navigation');
 
 function createOpenClass(element, className) {
   element.classList.add(className);
-};
-function createCloseClass(element, className){
-    element.classList.remove(className);
+}
+function createCloseClass(element, className) {
+  element.classList.remove(className);
 }
 
-openBtn.addEventListener('click', ()=> createOpenClass(mobMenu, 'menu-open'));
-closeBtn.addEventListener('click', ()=> createCloseClass(mobMenu, 'menu-open'));
+openBtn.addEventListener('click', () => createOpenClass(mobMenu, 'menu-open'));
+closeBtn.addEventListener('click', () => createCloseClass(mobMenu, 'menu-open'));
 
-const navBtns = document.querySelectorAll(".nav-btn");
+const navBtns = document.querySelectorAll('.nav-btn');
 navBtns.forEach((button) => {
-  button.addEventListener("click", () => createCloseClass(mobMenu, "menu-open"));
+  button.addEventListener('click', () => createCloseClass(mobMenu, 'menu-open'));
 });
