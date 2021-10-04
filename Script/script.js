42107c8e6658f5e7708cce6d9ecd95b04cb43285
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-04 22:57:04 +0500

    Add dynamics to Mob menu btn

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,9 @@
-const openBtn = document.querySelector("#menuBtn")
+const openBtn = document.querySelector("#menuBtn");
+const closeBtn = document.querySelector("#closeBtn");
+const mobMenu = document.querySelector("#mob-navigation");
+
+function createOpenClass(element, className) {
+  element.classList.add(className);
+};
+
+openBtn.addEventListener("click", ()=> createOpenClass(mobMenu, 'menu-open'));
