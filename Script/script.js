5d260775606715670592cd60a1b592c6e46b650c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-10-04 15:27:18 -0300

    Add action to menu btn

--- Script/script.js
+++ Script/script.js
@@ -5,5 +5,14 @@ const mobMenu = document.querySelector("#mob-navigation");
 function createOpenClass(element, className) {
   element.classList.add(className);
 };
+function createCloseClass(element, className){
+    element.classList.remove(className);
+}
 
-openBtn.addEventListener("click", ()=> createOpenClass(mobMenu, 'menu-open'));
+openBtn.addEventListener('click', ()=> createOpenClass(mobMenu, 'menu-open'));
+closeBtn.addEventListener('click', ()=> createCloseClass(mobMenu, 'menu-open'));
+
+const navBtns = document.querySelectorAll(".nav-btn");
+navBtns.forEach((button) => {
+  button.addEventListener("click", () => createCloseClass(mobMenu, "menu-open"));
+});
