0402e8ef81288384e2f0b4bb5b77322f05f0a92a
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-07 23:16:45 +0500

    Finish the styling validation and check linters

--- Script/validation.js
+++ Script/validation.js
@@ -1,13 +1,12 @@
 const contactForm = document.getElementById('contactMeForm');
 const emailInt = document.querySelector('#userEmail');
-const errMessage = document.querySelector("#errorMessage");
+const errMessage = document.querySelector('#errorMessage');
 
-contactForm.addEventListener('submit', (event)=> {
-  if(emailInt.value !== emailInt.value.toLowerCase()) {
+contactForm.addEventListener('submit', (event) => {
+  if (emailInt.value !== emailInt.value.toLowerCase()) {
     event.preventDefault();
-    errMessage.innerHTML = " * Please make sure all letters in email are lowercase";
+    errMessage.innerHTML = ' * Please make sure all letters in email are lowercase';
     errMessage.setAttribute('style', 'color: red; font-size: 10px;font-family: "Inter", sans-serif; ');
-    emailInt.setAttribute('style', 'border-bottom: 1px solid red;');
+    emailInt.setAttribute('style', 'border-bottom: 1px solid red; background: rgba(255, 0, 0, 0.2');
   }
-
-})
+});
