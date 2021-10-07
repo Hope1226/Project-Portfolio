cb2cc323fa688c1c6a47b43efbf052666eaf0039
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-08 03:43:12 +0500

    Restore data from localStorage

--- Script/localStorage.js
+++ Script/localStorage.js
@@ -1,24 +1,33 @@
-const userInfoForm = document.querySelector("#contactMeForm");
-const firstName = document.querySelector("#userFirstName");
-const lastName = document.querySelector("#userLastName");
+const userInfoForm = document.querySelector('#contactMeForm');
+const firstName = document.querySelector('#userFirstName');
+const lastName = document.querySelector('#userLastName');
 const userEmail = document.querySelector('#userEmail');
 const userMessage = document.querySelector('#message');
 
-
-let user = {};
+firstName.value = '';
+lastName.value = '';
+userEmail.value = '';
+userMessage.value = '';
 
 function storeUserData(event) {
   event.preventDefault();
+  const user = {
+    userFirstName: firstName.value,
+    userLastName: lastName.value,
+    userEmaiAdd: userEmail.value,
+    userTxtMsg: userMessage.value,
+  };
 
+  userInfoForm.submit();
 
+  localStorage.setItem('userData', JSON.stringify(user));
 }
 
+userInfoForm.addEventListener('submit', storeUserData);
 
-userInfoForm.addEventListener('submit', storeUserData(user));
+const userObject = JSON.parse(localStorage.getItem('userData'));
 
-let userData = JSON.stringify(user);
-
-localStorage.setItem('userData', userData);
- const dataFromStorage = localStorage.getItem('userData');
-
-
+firstName.value = userObject.userFirstName;
+lastName.value = userObject.userLastName;
+userEmail.value = userObject.userEmaiAdd;
+userMessage.value = userObject.userTxtMsg;
