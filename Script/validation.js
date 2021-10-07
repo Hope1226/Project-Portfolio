9c187c953e1e24dda12ebc53346d563e513d5741
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-10-07 15:08:50 -0300

    Add styles to validation

--- Script/validation.js
+++ Script/validation.js
@@ -5,8 +5,8 @@ const errMessage = document.querySelector("#errorMessage");
 contactForm.addEventListener('submit', (event)=> {
   if(emailInt.value !== emailInt.value.toLowerCase()) {
     event.preventDefault();
-    errMessage.innerHTML = "Please make sure all letters in email are lowercase";
-    errMessage.setAttribute('style', 'color: red; font-size: 16px;');
+    errMessage.innerHTML = " * Please make sure all letters in email are lowercase";
+    errMessage.setAttribute('style', 'color: red; font-size: 10px;font-family: "Inter", sans-serif; ');
     emailInt.setAttribute('style', 'border-bottom: 1px solid red;');
   }
 
